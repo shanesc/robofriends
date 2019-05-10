1bb7e3a2eea3fe7f2887453badc1a571ae045a44
Shane M <shaneSC@gmail.com>
2019-05-10 17:09:02 +0100

    refactored, reorganized folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import CardList from './CardList';
-import Scroll from './Scroll.js';
-import SearchBox from './SearchBox';
-import './App.css';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll.js';
+import SearchBox from '../components/SearchBox';
+import '../containers/App.css';
 
 
 class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
       robots: [],
       searchfield: '',
@@ -26,13 +25,13 @@ class App extends React.Component {
   }
 
   filterRobots = () => {
-    const filteredName = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredName = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    const filteredEmail = this.state.robots.filter(robot => {
-      return robot.email.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const filteredEmail = robots.filter(robot => {
+      return robot.email.toLowerCase().includes(searchfield.toLowerCase());
     });
-
     const filteredRobots = [].concat(filteredName).concat(filteredEmail).filter((value, i, arr) => {
       return arr.indexOf(value) === i;
     });
@@ -41,14 +40,13 @@ class App extends React.Component {
   }
 
   render() {
-    if (this.state.robots.length === 0) {
-      return (
+    return (!this.state.robots.length ?
+      (
         <div className='tc'>
           <h1 className='f1'>Loading...</h1>
         </div>
-      );
-    } else {
-      return (
+      ) :
+      (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -56,8 +54,8 @@ class App extends React.Component {
             <CardList robots={this.filterRobots()} />
           </Scroll>
         </div>
-      );
-    }
+      )
+    )
   }
 }
 
